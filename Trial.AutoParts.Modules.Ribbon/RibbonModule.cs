3ed1eee87758689de4aba2adf9e2d5903495d7f9
unknown <chirag_lad@PTD09704.persistent.co.in>
2016-10-20 18:18:52 +0530

    Ribbon Mudule integrated

---
+++ Trial.AutoParts.Modules.Ribbon/RibbonModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Trial.AutoParts.Modules.Ribbon
+{
+    public class RibbonModule : IModule
+    {
+        private readonly IRegionViewRegistry regionViewRegistry;
+
+        public RibbonModule(IRegionViewRegistry registry)
+        {
+            this.regionViewRegistry = registry;
+        }
+
+        public void Initialize()
+        {
+            regionViewRegistry.RegisterViewWithRegion("RibbonRegion", typeof(View.AutoPartsRibbon));
+        }
+    }
+}
---
+++ Trial.AutoParts/BootStraper.cs
@@ -0,0 +1,37 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Trial.AutoParts;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+using Trial.AutoParts.Modules.Ribbon;
+
+namespace Trial.AutoParts
+{
+    class BootStraper : UnityBootstrapper
+    {
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<PrismAppShell>();
+        }
+        protected override void InitializeModules()
+        {
+            base.InitializeModules();
+            App.Current.MainWindow = (PrismAppShell)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(RibbonModule));
+        }
+
+    }
+}
